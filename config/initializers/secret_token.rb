025ddfc11c0e8a07deb511d8b6c2f96c0849116a
Andrew Dennis <adennis4@gmail.com>
2012-04-03 14:58:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youquiz::Application.config.secret_token = '179a210afca81615f6dad18237fd82d19ff47fb02dd8c1a307084a171ce3d9f56a2e41559684d00e4e8ed1d82ca40d75dc09c6afca4b8c44c90349fe22969ad7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Youquiz::Application.config.session_store :cookie_store, key: '_youquiz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Youquiz::Application.config.session_store :active_record_store
